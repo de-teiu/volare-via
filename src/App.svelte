e5da8f34632ad1eeef659315440e8b092479f53f
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2020-02-03 00:37:09 +0900

    svelteとscss導入

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style type="text/scss">
+  .bora {
+    @for $i from 1 through 10 {
+      animation: "anime" + $i 1s ease ($i - 1) s alternate forwards;
+    }
+  }
+</style>
+
+<main>
+  <img class="bora" src="fish_bora.png" alt="bora" />
+</main>
